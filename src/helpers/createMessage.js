883b28a941d28314b717985245f1912335969241
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-05-16 13:17:00 +0100

    Change message creation method

--- src/helpers/createMessage.js
+++ src/helpers/createMessage.js
@@ -1,16 +1,15 @@
 module.exports = (username, labels = []) => {
   let message = '';
+  const ANIMAL_LABELS = ['Animal', 'Mammal', 'Bird', 'Fish', 'Reptile', 'Amphibian'];
+  const isAnimal = labels.length && labels.some(label => ANIMAL_LABELS.includes(label.Name));
 
   if (labels.length === 0) {
     message = `Sorry @${username}, you need to upload an image.`;
-  } else if (labels.filter((label) => {
-    const { Name } = label;
-    return Name === 'Animal' || Name === 'Mammal' || Name === 'Bird' || Name === 'Fish' || Name === 'Reptile' || Name === 'Amphibian';
-  }).length === 0) {
-    message = `Ooops @${username} looks like it's not an animal on your image.`;
-  } else {
+  } else if (isAnimal) {
     const recongizedLabels = labels.map(label => label.Name);
     message = `Hi @${username}. On your image, I can recognize: ${recongizedLabels.join(', ')}.`;
+  } else {
+    message = `Ooops @${username} looks like it's not an animal on your image.`;
   }
 
   return message;
